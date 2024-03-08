57a79c3aa223926352df4e5580e50c5f159ffe05
Zzzxb <idearfree@outlook.com>
2024-03-08 15:54:20 +0800

    算数运算

--- src/main/java/cn/tofucat/toolkit/util/FileUtils.java
+++ src/main/java/cn/tofucat/toolkit/util/FileUtils.java
@@ -7,7 +7,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.security.KeyFactory;
 import java.util.Collection;
 
 /**
--- src/test/java/cn/tofucat/toolkit/RSATest.java
+++ src/test/java/cn/tofucat/toolkit/RSATest.java
@@ -4,6 +4,7 @@ import cn.tofucat.toolkit.algorithm.RSA;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.File;
 import java.security.NoSuchAlgorithmException;
 
 /**
@@ -19,8 +20,8 @@ public class RSATest {
 
     @Test
     public void a () {
-        String encode = RSA.getInstance().encode("etc/pri.key", RSA.Key.PrivateKey, "你好");
-        String decode = RSA.getInstance().decode("etc/pub.key", RSA.Key.PublicKey, encode);
+        String encode = RSA.getInstance().encode(new File("etc/pri.key"), RSA.Key.PrivateKey, "你好");
+        String decode = RSA.getInstance().decode(new File("etc/pub.key"), RSA.Key.PublicKey, encode);
         System.out.println(decode);
     }
 }
